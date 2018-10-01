5ac5957c704ddd6de96d7f424ee7a2440e4a1548
Guillaume EHRET Dev-Mind <guillaume@dev-mind.fr>
2018-10-01 11:38:59 +0200

    Use a stable version of cheerio

--- extension/highlight-code.js
+++ extension/highlight-code.js
@@ -6,6 +6,24 @@ const cheerio = require('cheerio');
 
 module.exports = function ({ selector }) {
 
+  const updateComment = (html, language) => {
+    let startSeparator = '//';
+    let endSeparator = '';
+    switch (language) {
+      case 'shell':
+        startSeparator = '#';
+        break;
+      case 'html':
+        startSeparator = '<!--';
+        endSeparator = '-->';
+        break;
+    }
+
+    return html.replace(/<span class=\"token operator\">&lt;<\/span>b <span class=\"token keyword\">class<\/span><span class=\"token operator\">=<\/span><span class=\"token string\">\"conum\"<\/span><span class=\"token operator\">><\/span><span class=\"token punctuation\">\(<\/span><span class=\"token number\">/g, startSeparator + ' (')
+               .replace(/<\/span><span class=\"token punctuation\">\)<\/span><span class=\"token operator\">&lt;<\/span><span class=\"token operator\">\/<\/span>b<span class=\"token operator\">><\/span>/g, ')' + endSeparator);
+
+  };
+
   return map((file, next) => {
     const $ = cheerio.load(file.contents.toString(), { decodeEntities: false });
 
@@ -14,7 +32,7 @@ module.exports = function ({ selector }) {
       const language = elem.prop('data-lang');
       const fileContents = elem.html();
       const highlightedContents = Prism.highlight(fileContents,  Prism.languages[language] || Prism.languages.autoit);
-      elem.parent().replaceWith( `<pre class="language-${language}">${highlightedContents}</pre>`);
+      elem.parent().replaceWith( `<pre class="language-${language}">${updateComment(highlightedContents)}</pre>`);
       elem.addClass('highlights');
     });
 

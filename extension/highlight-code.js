2fa664a231cdce7173fd0cd29394431be84afe99
Guillaume EHRET <guillaume@dev-mind.fr>
2020-10-07 09:00:24 +0200

    Upgrade Primsjs lib

--- extension/highlight-code.js
+++ extension/highlight-code.js
@@ -2,43 +2,20 @@
 exports.__esModule = true;
 exports.extHighlightCode = void 0;
 var through2 = require("through2");
-var Prism = require('node-prismjs');
+var Prism = require('prismjs');
 var cheerio = require('cheerio');
+var loadLanguages = require('prismjs/components/');
 function extHighlightCode(_a) {
     var selector = _a.selector;
-    var updateComment = function (html, language) {
-        var startSeparator = '//';
-        var endSeparator = '';
-        switch (language) {
-            case 'shell':
-                startSeparator = '#';
-                break;
-            case 'html':
-                startSeparator = '<!--';
-                endSeparator = '-->';
-                break;
-        }
-        return html.replace(/<span class=\"token operator\">&lt;<\/span>b <span class=\"token keyword\">class<\/span><span class=\"token operator\">=<\/span><span class=\"token string\">\"conum\"<\/span><span class=\"token operator\">><\/span><span class=\"token punctuation\">\(<\/span><span class=\"token number\">/g, startSeparator + ' (')
-            .replace(/<span class=\"token operator\">=<\/span><span class=\"token operator\">&amp;<\/span>gt<span class=\"token punctuation\">;<\/span>/g, '=>')
-            .replace(/<span class=\"token punctuation\">\)<\/span><span class=\"token operator\">&lt;<\/span><span class=\"token operator\">\/<\/span>b<span class=\"token operator\">><\/span>/g, ')' + endSeparator);
-    };
-    var updateJava = function (html, language) {
-        if (language === 'java' || language === 'Java') {
-            return html.replace(/<span class=\"token operator\">&amp;<\/span>lt<span class=\"token punctuation\">;<\/span>/g, '&lt;')
-                .replace(/<span class=\"token operator\">&amp;<\/span>gt<span class=\"token punctuation\">;<\/span>/g, '&gt;');
-        }
-        return html;
-    };
     return through2.obj(function (file, _, next) {
         var $ = cheerio.load(file.contents.toString(), { decodeEntities: false });
         $(selector).each(function (index, code) {
             var elem = $(code);
             var language = elem.prop('data-lang');
             var fileContents = elem.html();
-            var highlightedContents = Prism.highlight(fileContents, Prism.languages[language] || Prism.languages.autoit);
-            var htmlWithComments = updateComment(highlightedContents, language);
-            var finalHtml = updateJava(htmlWithComments, language);
-            elem.parent().replaceWith("<pre class=\"language-" + language + "\">" + finalHtml + "</pre>");
+            loadLanguages(language);
+            var highlightedContents = Prism.highlight(fileContents, Prism.languages[language], language);
+            elem.parent().replaceWith("<pre class=\"language-" + language + "\">" + highlightedContents + "</pre>");
             elem.addClass('highlights');
         });
         file.contents = Buffer.from($.html());
--- extension/highlight-code.ts
+++ extension/highlight-code.ts
@@ -4,38 +4,11 @@ import {Transform} from 'stream';
 import * as through2 from 'through2';
 import {TransformCallback} from 'through2';
 
-const Prism = require('node-prismjs');
+const Prism = require('prismjs');
 const cheerio = require('cheerio');
+const loadLanguages = require('prismjs/components/');
 
 export function extHighlightCode({selector}): Transform {
-
-  const updateComment = (html, language) => {
-    let startSeparator = '//';
-    let endSeparator = '';
-    switch (language) {
-      case 'shell':
-        startSeparator = '#';
-        break;
-      case 'html':
-        startSeparator = '<!--';
-        endSeparator = '-->';
-        break;
-    }
-
-    return html.replace(/<span class=\"token operator\">&lt;<\/span>b <span class=\"token keyword\">class<\/span><span class=\"token operator\">=<\/span><span class=\"token string\">\"conum\"<\/span><span class=\"token operator\">><\/span><span class=\"token punctuation\">\(<\/span><span class=\"token number\">/g, startSeparator + ' (')
-      .replace(/<span class=\"token operator\">=<\/span><span class=\"token operator\">&amp;<\/span>gt<span class=\"token punctuation\">;<\/span>/g, '=>')
-      .replace(/<span class=\"token punctuation\">\)<\/span><span class=\"token operator\">&lt;<\/span><span class=\"token operator\">\/<\/span>b<span class=\"token operator\">><\/span>/g, ')' + endSeparator);
-
-
-  };
-
-  const updateJava = (html, language) => {
-    if (language === 'java' || language === 'Java') {
-      return html.replace(/<span class=\"token operator\">&amp;<\/span>lt<span class=\"token punctuation\">;<\/span>/g, '&lt;')
-        .replace(/<span class=\"token operator\">&amp;<\/span>gt<span class=\"token punctuation\">;<\/span>/g, '&gt;')
-    }
-    return html;
-  };
 
   return through2.obj((file, _, next: TransformCallback) => {
     const $ = cheerio.load(file.contents.toString(), {decodeEntities: false});
@@ -44,10 +17,9 @@ export function extHighlightCode({selector}): Transform {
       const elem = $(code);
       const language = elem.prop('data-lang');
       const fileContents = elem.html();
-      const highlightedContents = Prism.highlight(fileContents, Prism.languages[language] || Prism.languages.autoit);
-      const htmlWithComments = updateComment(highlightedContents, language);
-      const finalHtml = updateJava(htmlWithComments, language);
-      elem.parent().replaceWith(`<pre class="language-${language}">${finalHtml}</pre>`);
+      loadLanguages(language)
+      const highlightedContents = Prism.highlight(fileContents, Prism.languages[language], language);
+      elem.parent().replaceWith(`<pre class="language-${language}">${highlightedContents}</pre>`);
       elem.addClass('highlights');
     });
 
